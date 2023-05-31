de1b52bb1819a47087dd0be53201d1c2cb4a4977
Tetiana <tanyuwik777@gmail.com>
2023-05-31 16:49:10 +0200

    Added Bootstrap

--- src/Weather.js
+++ src/Weather.js
@@ -14,64 +14,66 @@ export default function Weather() {
 
   return (
     <div className="Weather">
-      <form className="mb-3">
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Type a city"
-              className="form-control"
-              autoComplete="off"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <div className="forecast">
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>{weatherData.date}</li>
-          <li>{weatherData.description}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="d-flex weather-temperature">
-            <img
-              src={weatherData.imgUrl}
-              alt={weatherData.description}
-              className="float-left"
-            />
-            <div className="float-left">
-              <span className="current-temperature">
-                {weatherData.temperature}
-              </span>
-              <span className="units">°C</span>
+      <div class="container">
+        <form className="mb-3">
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Type a city"
+                className="form-control"
+                autoComplete="off"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
-        <div className="col-6">
+        </form>
+        <div className="forecast">
+          <h1>{weatherData.city}</h1>
           <ul>
-            <li>Humidity: {weatherData.humidity} %</li>
-            <li>Wind: {weatherData.wind} km/h</li>
+            <li>{weatherData.date}</li>
+            <li>{weatherData.description}</li>
           </ul>
         </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="d-flex weather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <div className="float-left">
+                <span className="current-temperature">
+                  {weatherData.temperature}
+                </span>
+                <span className="units">°C</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity} %</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
+        <div className="url-code mt-4"></div>
+        <a
+          href="https://github.com/Tanyuwik/weather-react"
+          target="_blank"
+          rel="noreferrer"
+          className="website-url">
+          Open-source code
+        </a>
+        <span className="name "> by Tetiana Zlyhostieva</span>
       </div>
-      <div className="url-code mt-4"></div>
-      <a
-        href="https://github.com/Tanyuwik/weather-react"
-        target="_blank"
-        rel="noreferrer"
-        className="website-url">
-        Open-source code
-      </a>
-      <span className="name "> by Tetiana Zlyhostieva</span>
     </div>
   );
 }
